f64ae0135ccc4cf0b5b8292320bf297243fd8860
bpasn <57083511+bpasn@users.noreply.github.com>
2021-07-07 09:19:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+import CartScreen from "./screens/CartScreen";
+import { useDispatch, useSelector } from "react-redux";
+import SigninScreen from "./screens/SigninScreen";
+import { signout } from "./actions/useAction";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
+import OrderHostoryScreen from "./screens/OrderHostoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
+  
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              Amazona
+            </Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                <li>
+                  <Link to='/profile' >
+                   User profile
+                  </Link>
+                  </li>
+                  <li>
+                  <Link to='/orderhistory' >
+                    Order History
+                  </Link>
+                  </li>
+                 <li>
+                 <Link to='#signout' onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                 </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign in</Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <Link to='#admin'>Admin {' ' } <i className='fa fa-caret-down'></i></Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to='/dashboard'>Dashboard</Link>
+                  </li>
+                  <li>
+                    <Link to='/products'>Products</Link>
+                  </li>
+                  <li>
+                    <Link to='/orderlist'>Orders</Link>
+                  </li>
+                  <li>
+                    <Link to='/userlist'>User</Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/product/:id?" component={ProductScreen} />
+          <Route path="/signin" component={SigninScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/shipping" component={ShippingAddressScreen} />
+          <Route path="/payment" component={PaymentMethodScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/orderhistory" component={OrderHostoryScreen} />
+          <PrivateRoute path="/profile" component={ProfileScreen} />
+          <Route path="/" component={HomeScreen} exact />
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
