789b1f62d536ef0473a6b17f45989787c79340ea
bpasn <57083511+bpasn@users.noreply.github.com>
2021-07-07 09:18:55 +0700

    Create productReducers.js

---
+++ src/reducers/productReducers.js
@@ -0,0 +1,48 @@
+// const {
+//   PRODUCT_LIST_FAIL,
+//   PRODUCT_LIST_REQUEST,
+//   PRODUCT_LIST_SUCCESS,
+//   PRODUCT_DETAIL_REQUEST,
+//   PRODUCT_DETAIL_SUCCESS,
+//   PRODUCT_DETAIL_FAIL,
+// } = require("../constants/productConstants");
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
+} from "../constants/productConstants";
+//paramitor State ตัวนี้ เป็นค่าเริ่มต้นของstateที่จะreturnค่าออกไป ในนี้จะมี obj เป็น{loading: true, products:[]}
+export const productListReducer = (
+  state = { loading: true, products: [] },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true };
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailReducer = (
+  state = { loading: true, products: {} },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAIL_REQUEST:
+      return { loading: true };
+    case PRODUCT_DETAIL_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAIL_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
